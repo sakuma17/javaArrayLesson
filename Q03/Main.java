3714df9e00bfd4d92128e63401c864190b2cc9be
sakuma17 <ids08663@gmail.com>
2020-11-28 16:42:29 +0900

    Q01~10解答

--- Q03/Main.java
+++ Q03/Main.java
@@ -2,8 +2,10 @@ import java.util.*;
 public class Main{
 	public static void main(String[] args){
 		//要素数3のdouble配列作成
+		double[] nums=new double[3];
 		for(int i=0;i<nums.length;i++){
 			//1.0から順番に詰める
+			nums[i]=i+1.0;
 		}
 		System.out.println(Arrays.toString(nums));
 	}
--- Q08/Main.java
+++ Q08/Main.java
@@ -4,10 +4,12 @@ public class Main{
 		System.out.print("何回サイコロをふる>");
 		int num=new Scanner(System.in).nextInt();
 		//boolean配列を作成する(配列変数名はisEven)
+		boolean[] isEven=new boolean[num];
 		for(int i=0;i<isEven.length;i++){
 			int dice=new Random().nextInt(6)+1;
 			System.out.println(dice);
 			//配列に偶数だったらtrue,奇数だったらfalseを代入
+			isEven[i]=dice%2==0?true:false;
 		}
 		System.out.println(Arrays.toString(isEven));
 	}
